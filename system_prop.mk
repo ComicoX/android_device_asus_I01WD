7c42b1d900d5a965186c634080707a2f42a34415
ComicoTeam <comico_work@outlook.com>
2020-02-15 20:38:09 +0200

    I01WD: remove media_profiles_vendor.xml

--- system_prop.mk
+++ system_prop.mk
@@ -76,7 +76,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-    media.settings.xml=/system/etc/media_profiles_vendor.xml
+    media.settings.xml=/vendor/etc/media_profiles_vendor.xml
 
 # Netmgr
 PRODUCT_PROPERTY_OVERRIDES += \
