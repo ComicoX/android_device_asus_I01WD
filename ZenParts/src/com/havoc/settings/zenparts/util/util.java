15cbfacf81d054146352c786756ee54d02d211fc
Raileanu Cosmin <comico_work@outlook.com>
2020-06-09 22:26:06 +0300

    Zenparts: Derp again

--- ZenParts/src/com/havoc/settings/zenparts/util/util.java
+++ ZenParts/src/com/havoc/settings/zenparts/util/util.java
@@ -24,7 +24,7 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class Utils {
+public class util {
 
     /**
      * Write a string value to the specified file.
